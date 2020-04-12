0b93189a0bf47d7a843eb9f0344455dd7781fbed
Irelion <irelion@mail.ru>
2020-04-12 16:34:23 +0300

    1. Конструктор по умолчанию.[#262368]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,13 +3,26 @@ package ru.job4j.tracker;
 import java.util.Objects;
 
 public class Item {
+    private String id;
+    private String name;
 
-    public Item() {
-        super();
-        System.out.println("load Item");
+    public Item(String name) {
+        this.name = name;
     }
 
-    public static void main(String[] args) {
-        Item item = new Item();
-        }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
