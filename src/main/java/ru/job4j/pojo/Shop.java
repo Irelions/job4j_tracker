4e3ebae21eae73cca78a62b803d8f020a212baa9
Irelion <irelion@mail.ru>
2020-04-14 14:00:52 +0300

    3. Удаление моделей из массива.[#262378]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public Product[] delete(Product[] products, int index) {
+        products[index] = null;
+
+        for (int i = index + 1; i < products.length; i++) {
+            products[i - 1] = products[i];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
--- src/main/java/ru/job4j/pojo/Store.java
+++ src/main/java/ru/job4j/pojo/Store.java
@@ -6,32 +6,56 @@ public class Store {
         Product bread = new Product("Bread", 4);
         Product egg = new Product("Egg", 19);
 
-        Product[] prods = new Product[3];
+        Product[] prods = new Product[5];
 
         prods[0] = milk;
         prods[1] = bread;
         prods[2] = egg;
 
+
         for (int index = 0; index < prods.length; index++) {
-            Product pr = prods[index];
-            System.out.println(pr.getName() + " - " + pr.getCount());
+            Product product = prods[index];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
         }
 
-        System.out.println('\n' + "Replace milk to oil");
-        Product oil = new Product("Oil", 11);
-        prods[0] = oil;
-
+        System.out.println('\n' + "Удаляем значение из ячейки с индексом 1");
+        prods[1] = null;
         for (int index = 0; index < prods.length; index++) {
-            Product pr = prods[index];
-            System.out.println(pr.getName() + " - " + pr.getCount());
+            Product product = prods[index];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
         }
 
-        System.out.println('\n' + "Shown only product > 10");
+        System.out.println('\n' + "Записывем в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение ячейки с индексом 2");
+        prods[1] = prods[2];
+        prods[2] = null;
         for (int index = 0; index < prods.length; index++) {
-            Product pr = prods[index];
-            if (prods[index].getCount() > 10) {
-                System.out.println(pr.getName() + " - " + pr.getCount());
+            Product product = prods[index];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println('\n' + "Удаляем через метод delete");
+        Shop shop = new Shop();
+        prods = shop.delete(prods, 0);
+        for (int index = 0; index < prods.length; index++) {
+            Product product = prods[index];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
             }
         }
     }
 }
+
