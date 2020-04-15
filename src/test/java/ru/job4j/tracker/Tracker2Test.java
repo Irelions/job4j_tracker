8700e8d1827866e7ab1f499478e721a4fbb5c6ec
Irelion <irelion@mail.ru>
2020-04-15 18:23:43 +0300

    5. Реализовать класс Tracker[#262382] - подготовка к заданию

--- src/test/java/ru/job4j/tracker/Tracker2Test.java
+++ src/test/java/ru/job4j/tracker/Tracker2Test.java
@@ -2,15 +2,15 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import ru.job4j.Item;
-import ru.job4j.Tracker;
+import ru.job4j.Tracker2;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class TrackerTest {
+public class Tracker2Test {
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
-        Tracker tracker = new Tracker();
+        Tracker2 tracker = new Tracker2();
         Item item = new Item("test1");
         tracker.add(item);
         Item result = tracker.findById(item.getId());
