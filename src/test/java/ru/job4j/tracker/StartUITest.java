921f8705ad00bf25366c9b690174632c368fa008
Irelion <irelion@mail.ru>
2020-04-22 22:26:39 +0300

    5.1. Тестирование. Подготовка данных.[#262332]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.rmi.server.UnicastRemoteObject;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -15,5 +18,30 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void testEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("New Item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(),
+                "replace item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replace = tracker.findById(item.getId());
+        assertThat(replace.getName(), is("replace item"));
+    }
+
+    @Test
+    public void testDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("New Item");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item delete = tracker.findById(answers[0]);
+        assertEquals(delete, null);
     }
 }
