bd9f4359dd44f83ee4047143c0084426149d796b
Irelion <irelion@mail.ru>
2020-04-26 14:47:05 +0300

    9. Написать тесты на StartUI.[#262335]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,47 +1,20 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
-
-import java.rmi.server.UnicastRemoteObject;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-
     @Test
-    public void testCreateItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
 
-    @Test
-    public void testEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("New Item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(),
-                "replace item"
-        };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item replace = tracker.findById(item.getId());
-        assertThat(replace.getName(), is("replace item"));
-    }
-
-    @Test
-    public void testDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("New Item");
-        tracker.add(item);
-        String[] answers = {item.getId()};
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item delete = tracker.findById(answers[0]);
-        assertEquals(delete, null);
+        new StartUI().init(input, new Tracker(), new UserAction[] {
+                action
+        });
+        assertThat(action.isCall(), is(true));
     }
 }
