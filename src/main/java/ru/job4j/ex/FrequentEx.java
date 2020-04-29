8bb73879f6d2ad5c929c1ff0e075a5b83df56299
irelion <irelion@mail.ru>
2020-04-29 08:55:48 +0300

    0. Что такое исключение. [#262349]

---
+++ src/main/java/ru/job4j/ex/FrequentEx.java
@@ -0,0 +1,10 @@
+package ru.job4j.ex;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shop = {"Ebay", "Amazon", "Ozon"};
+        for (int index = 0; index < shop.length; index++) {
+            System.out.println(shop[index]);
+        }
+    }
+}
