dc9435014488ab93ad95ac535094141ed8169dc6
Irelion <irelion@mail.ru>
2020-05-03 11:34:40 +0300

    3.1. Приведение типов. Повышение типа и понижение типа.[#262328]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,27 +4,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Matches {
-    public static void main(String[] args) {
+    public static int subMatches(String name, int matches) {
         Scanner input = new Scanner(System.in);
+        System.out.println("В стопке " + matches + " спичек. Возьмите от 1 до 3 спичек.");
+        System.out.print(name + ": ");
+        matches -= Integer.valueOf(input.nextLine());
+        return matches;
+    }
+
+    public static boolean checkWin(int matches) {
+        boolean win = false;
+        if (matches == 0 || matches < 0) {
+            win = true;
+        }
+        return win;
+    }
+
+
+    public static void main(String[] args) {
+        String player1 = "Player 1";
+        String player2 = "Player 2";
+        boolean win = false;
         int matches = 11;
 
         System.out.println(" * * * ИГРА \"11\" * * *");
         while (matches > 0) {
-            System.out.println("В стопке " + matches + " спичек. Возьмите от 1 до 3 спичек.");
-            System.out.print("Player 1: ");
-            matches -= Integer.valueOf(input.nextLine());
-            if (matches == 0 || matches < 0) {
-                System.out.println("Победитель - Player 1");
+            win = checkWin(subMatches(player1, matches));
+            if (win) {
+                System.out.println(player1 + " победитель!");
                 break;
             }
-            System.out.println("В стопке " + matches + " спичек. Возьмите от 1 до 3 спичек.");
-            System.out.print("Player 2: ");
-            matches -= Integer.valueOf(input.nextLine());
-            if (matches == 0 || matches < 0) {
-                System.out.println("Победитель - Player 2");
+            win = checkWin(subMatches(player2, matches));
+            if (win) {
+                System.out.println(player2 + " победитель!");
                 break;
             }
-            System.out.println();
         }
     }
 }
